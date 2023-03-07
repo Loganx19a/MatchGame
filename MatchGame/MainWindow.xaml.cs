13b7254561e1cd058cfe11bf203c8582f834868f
Logan Smith <loganx19a@ksu.edu>
2023-03-07 05:35:33 -0600

    Add project files.

---
+++ MatchGame/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿/* MainWindow.xaml.cs
+ * Author: Logan Smith
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MatchGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetUpGame();
+        }
+
+        /// <summary>
+        /// A method that sets up the matching game by populating the first 16 cells in the mainGrid with a randomly selected emoji
+        /// </summary>
+        private void SetUpGame()
+        {
+            List<string> animalEmoji = new List<string>()
+            {
+                // 🐕 🐈‍ 🐅 🦛 🐘 🐢 🦑 🦞 🐳 🦦 🦀 🦖
+                
+                "🐕", "🐕",
+                "‍🦖", "‍🦖",
+                "🦦", "🦦",
+                "🦛", "🦛",
+                "🐘", "🐘",
+                "🐢", "🐢",
+                "🦑", "🦑",
+                "🐳", "🐳"
+            };
+
+            Random random = new Random();
+
+            foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
+            {
+                int index = random.Next(animalEmoji.Count);
+                string nextEmoji = animalEmoji[index];
+                textBlock.Text = nextEmoji;
+                animalEmoji.RemoveAt(index);
+            }
+        }
+    }
+}
